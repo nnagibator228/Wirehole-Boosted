3e02907d0ce9a8fdc0003298dc7b787e476244cd
plzdontcry <63839121+nnagibator228@users.noreply.github.com>
2022-10-14 19:50:35 +0300

    initial commit

---
+++ install_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -yqq \
+curl \
+git \
+apt-transport-https \
+ca-certificates \
+gnupg-agent \
+software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable" &&
+sudo apt-get update &&
+sudo apt-get install docker-ce docker-ce-cli containerd.io -yqq
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+sudo chmod +x /usr/local/bin/docker-compose &&
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo mkdir /etc/{wireguard,ipsec,pihole,unbound}
+mv unbound.conf /etc/unbound/
+docker-compose up
