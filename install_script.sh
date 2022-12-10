e64d4d303f016ab7592331f989f5d66d0dfa077b
plzdontcry <zmater2005@gmail.com>
2022-12-10 17:12:34 +0300

    Traefik Rproxy with BasicAuth added for web dashboards. Now web dashboards are available even without connecting to vpn's internal network.

--- install_script.sh
+++ install_script.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
+usern="test123"
+passwd="TestForVPN123"
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -u|--user)
+      usern="$2"
+      shift
+      shift
+      ;;
+    -p|--passwd)
+      passwd="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") #
+      shift
+      ;;
+  esac
+done
+
+passwd_hash=$(htpasswd -bnBC 10 "" $passwd | tr -d ':\n' | sed 's/\$/$$/g')
+
 sudo apt-get update && sudo apt-get install -yqq \
 curl \
 git \
 apt-transport-https \
 ca-certificates \
 gnupg-agent \
-software-properties-common
+software-properties-common \
+apache2-utils
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -22,7 +53,12 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose &&
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-sudo mkdir /etc/{wireguard,ipsec,pihole,unbound,prometheus}
+sudo mkdir /etc/{wireguard,ipsec,pihole,unbound,prometheus,grafana}
 mv unbound.conf /etc/unbound/
 mv prometheus.yml /etc/prometheus/
-docker-compose up
+mv grafana.ini /etc/grafana/
+
+sed -i "s/<username>/$usern/" docker-compose.yml
+sed -i "s/<passwdhash>/$passwd_hash/" docker-compose.yml
+
+docker-compose up -d
